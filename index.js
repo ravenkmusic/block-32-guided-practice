1b9ead0ed268945902e3c104530ef08d91434d74
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-01 20:14:23 -0400

    added logic to CRUD routes

--- index.js
+++ index.js
@@ -10,11 +10,66 @@ app.use(express.json()); //parsing requests from incoming requests
 app.use(require('morgan')('dev')) // logs requests as they come
 
 //CRUD operations routes
-app.post('/api/notes', async (req, res, next) => {}); //create
-app.get('/api/notes', async (req, res, next) => {}) // read
-app.put('/api/notes/:id', async (req, res, next) => {}) //update
-app.delete('/api/notes/:id', async (req, res, next) => {}) // delete
 
+//create
+
+app.post('/api/notes', async (req, res, next) => {
+    try {
+        const SQL = `
+            INSERT INTO notes (text) VALUES($1) RETURNING *
+            `;
+        const response = await client.query(SQL, [req.body.txt]);
+        res.send(response.rows[0]);
+    } catch (ex) {
+        
+    }
+});
+
+// read
+
+app.get('/api/notes', async (req, res, next) => {
+    try {
+        const SQL = `SELECT * from notes ORDER BY created_at DESC`;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (ex) {
+        console.error(ex);
+    }
+});
+
+//update
+
+app.put('/api/notes/:id', async (req, res, next) => {
+    try {
+        const SQL = `
+            UPDATE notes
+
+            SET txt=$1, ranking=$2, upated_at=now()
+            
+            WHERE id=$3 RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.txt, req.body.ranking, req.params.id]);
+        res.send(response.rows[0])
+    } catch (ex) {
+        console.error(ex);
+    }
+})
+
+// delete
+
+app.delete('/api/notes/:id', async (req, res, next) => {
+    try {
+        const SQL = `
+            DELETE from notes
+
+            WHERE id=$1
+        `;
+        const response = await client.query(SQL, []);
+        res.sendStatus(204);
+    } catch (ex) {
+        console.error(ex);
+    }
+})
 
 
 //connect to database
@@ -42,8 +97,8 @@ async function init(){
         INSERT INTO notes (txt, ranking) VALUES('Note 3', 3);
         `;
     console.log('data seeded.');
-    const port = process.env.PORT || 3000;
-    app.listen(port, ()=> console.log(`Now listening on port ${port}.`));
+    const port = process.env.PORT || 3000; // variable for port
+    app.listen(port, ()=> console.log(`Now listening on port ${port}.`)); //listens on port 3000
 }
 
 //invoking init function
