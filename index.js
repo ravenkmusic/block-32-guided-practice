edcbb1370033dbce55657f72b5cf1771729ed43e
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-04 15:21:30 -0400

    fixed LOTS of typos

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db');
 const express = require('express');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db')
-const app = express()
+const app = express();
 
 
 //deployment routes
 
 app.use(express.json()); //parsing requests from incoming requests
-app.use(require('morgan')('dev')) // logs requests as they come
+app.use(require('morgan')('dev')); // logs requests as they come
 
 //CRUD operations routes
 
@@ -16,12 +16,14 @@ app.use(require('morgan')('dev')) // logs requests as they come
 app.post('/api/notes', async (req, res, next) => {
     try {
         const SQL = `
-            INSERT INTO notes (txt) VALUES($1) RETURNING *
+            INSERT INTO notes (txt)
+            VALUES($1)
+            RETURNING *
             `;
-        const response = await client.query(SQL, [req.body.txt, req.body.ranking]);
+        const response = await client.query(SQL, [req.body.txt]);
         res.send(response.rows[0]);
-    } catch (error) {
-        next(error);
+    } catch (ex) {
+        next(ex);
     }
 });
 
@@ -29,11 +31,13 @@ app.post('/api/notes', async (req, res, next) => {
 
 app.get('/api/notes', async (req, res, next) => {
     try {
-        const SQL = `SELECT * from notes ORDER BY created_at DESC`;
+        const SQL = `
+            SELECT * FROM notes ORDER BY created_at DESC
+            `;
         const response = await client.query(SQL);
         res.send(response.rows);
-    } catch (error) {
-        next(error);
+    } catch (ex) {
+        next(ex);
     }
 });
 
@@ -43,17 +47,13 @@ app.put('/api/notes/:id', async (req, res, next) => {
     try {
         const SQL = `
             UPDATE notes
-
-            SET txt=$1, ranking=$2, updated_at=now()
-            
-            WHERE id=$3 
-            
-            RETURNING *
+            SET txt=$1, ranking=$2, updated_at=now()            
+            WHERE id=$3 RETURNING *
         `;
         const response = await client.query(SQL, [req.body.txt, req.body.ranking, req.params.id]);
-        res.send(response.rows[0])
-    } catch (error) {
-        next(error);
+        res.send(response.rows[0]);
+    } catch (ex) {
+        next(ex);
     }
 });
 
@@ -63,46 +63,45 @@ app.delete('/api/notes/:id', async (req, res, next) => {
     try {
         const SQL = `
             DELETE from notes
-
             WHERE id= $1
         `;
-        await client.query(SQL, [req.params.id]);
+        const response = await client.query(SQL, [req.params.id]);
         res.sendStatus(204);
-    } catch (error) {
-        next(error);
+    } catch (ex) {
+        next(ex);
     }
 });
 
 
 //connect to database
 
-async function init(){
+const init = async() => {
     await client.connect();
     console.log('Connected to database');
     
     //creating the tables
     let SQL = `
-        DROP TABLE IF EXISTS notes
+        DROP TABLE IF EXISTS notes;
         CREATE TABLE notes(
             id SERIAL PRIMARY KEY,
             created_at TIMESTAMP DEFAULT now(),
             updated_at TIMESTAMP DEFAULT now(),
             ranking INTEGER DEFAULT 3 NOT NULL,
             txt VARCHAR(255) NOT NULL
-            );
+            )
         `;
     await client.query(SQL);
     console.log('tables created');
     SQL = `
-        INSERT INTO notes (txt, ranking) VALUES('Note 1', 5);
-        INSERT INTO notes (txt, ranking) VALUES('Note 2', 1);
-        INSERT INTO notes (txt, ranking) VALUES('Note 3', 3);
+        INSERT INTO notes (txt, ranking) VALUES('learn express', 5);
+        INSERT INTO notes (txt, ranking) VALUES('write SQL queries', 4);
+        INSERT INTO notes (txt, ranking) VALUES('create routes', 2);
         `;
     await client.query(SQL);
     console.log('data seeded.');
-    const port = process.env.PORT || 3000; // variable for port
+    const port = process.env.PORT || 3000 // variable for port
     app.listen(port, ()=> console.log(`Now listening on port ${port}.`)); //listens on port 3000
-}
+};
 
 //invoking init function
 init();
